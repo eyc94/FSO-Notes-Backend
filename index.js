9986988e51ff5dbe74b911d621fcfd8fdaefcb8b
Eric Chin <ericychin94@gmail.com>
2022-01-10 18:10:55 -0800

    Added update route handler

--- index.js
+++ index.js
@@ -68,6 +68,21 @@ app.post('/api/notes', (request, response) => {
     });
 });
 
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body;
+
+    const note = {
+        content: body.content,
+        important: body.important
+    };
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote);
+        })
+        .catch(error => next(error));
+});
+
 const unknownEndpoint = (request, response) => {
     console.log('error');
     response.status(404).send({ error: 'unknown endpoint' });
