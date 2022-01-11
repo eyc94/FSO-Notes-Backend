51a9da69b5e990ad43f8d95dd3116cf44acf6782
Eric Chin <ericychin94@gmail.com>
2022-01-10 18:06:49 -0800

    Error handling middleware

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/api/notes', (request, response) => {
     });
 });
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id)
         .then(note => {
             if (note) {
@@ -37,10 +37,7 @@ app.get('/api/notes/:id', (request, response) => {
                 response.status(404).end();
             }
         })
-        .catch(error => {
-            console.log(error);
-            response.status(400).send({ error: 'malformatted id' });
-        });
+        .catch(error => next(error));
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -75,6 +72,18 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
+    }
+
+    next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
