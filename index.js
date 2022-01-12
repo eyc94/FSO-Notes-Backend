d42ea46fab80494697d39fa0b304c39f4d240f5b
Eric Chin <ericychin94@gmail.com>
2022-01-12 15:00:09 -0800

    Added catch block to POST route and added condition to error handler

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.delete('/api/notes/:id', (request, response, next) => {
         .catch(error => next(error));
 });
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body;
 
     if (body.content === undefined) {
@@ -63,9 +63,11 @@ app.post('/api/notes', (request, response) => {
         date: new Date(),
     });
 
-    note.save().then(savedNote => {
-        response.json(savedNote);
-    });
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote);
+        })
+        .catch(error => next(error));
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -95,6 +97,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' });
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message });
     }
 
     next(error);
