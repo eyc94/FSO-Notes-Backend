35043387b3e42546b04bf5151c6b315055902843
Eric Chin <ericychin94@gmail.com>
2022-01-14 11:46:44 -0800

    Cleaned index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Start New.
 const app = require('./app');   // The actual Express application.
 const http = require('http');
 const config = require('./utils/config');
@@ -9,32 +8,3 @@ const server = http.createServer(app);
 server.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`);
 });
-// End New.
-
-
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method);
-    console.log('Path:  ', request.path);
-    console.log('Body:  ', request.body);
-    console.log('---');
-    next();
-};
-
-
-const unknownEndpoint = (request, response) => {
-    console.log('error');
-    response.status(404).send({ error: 'unknown endpoint' });
-};
-
-
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message);
-
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' });
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message });
-    }
-
-    next(error);
-};
