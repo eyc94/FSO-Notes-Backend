e45468d670f607b6f5ec6d6f55e9050677b6ab28
Eric Chin <ericychin94@gmail.com>
2022-02-10 23:38:26 -0800

    Used validator in user schema

--- models/user.js
+++ models/user.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
-    username: String,
+    username: {
+        type: String,
+        unique: true
+    },
     name: String,
     passwordHash: String,
     notes: [
